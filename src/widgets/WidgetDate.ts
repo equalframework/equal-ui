622978e67c08bd86aec30da26cda96e33e77bfe0
Cédric Françoys <cedricfrancoys@gmail.com>
2024-03-23 11:43:26 +0100

    added support + fix date formatting

--- src/widgets/WidgetDate.ts
+++ src/widgets/WidgetDate.ts
@@ -14,25 +14,18 @@ export default class WidgetDate extends Widget {
     }
 
     public change(value: any) {
+        console.log('WidgetDate::change', value);
         this.$elem.find('input').datepicker('setDate', value).trigger('change');
     }
 
     public render(): JQuery {
         let date = new Date(this.value);
         let value:any;
-         // moment 'll': en = Jul 8, 2023; fr = 8 juil. 2023
+         // moment 'll': en = "Jul 8, 2023"; fr = "8 juil. 2023"
         let format = 'll';
         switch(this.mode) {
             case 'edit':
-                format = 'L';
-                value = moment(date).format(format);
-                this.$elem = UIHelper.createInput('date_'+this.id, this.label, value, this.config.description, 'calendar_today', this.readonly);
-                if(this.config.layout == 'list') {
-                    this.$elem.css({"width": "calc(100% - 10px)"});
-                }
-                // setup handler for relaying value update to parent layout
-                this.$elem.find('input')
-                .datepicker( {
+                let datepickerConfig = {
                     showOn: "button",
                     ...jqlocale[this.getLayout().getEnv().locale],
                     onClose: () => {
@@ -41,11 +34,41 @@ export default class WidgetDate extends Widget {
                             $('#'+this.getId()).find('input').first().trigger('focus');
                         }, 250);
                     }
-                } )
+                };
+                if(this.config.hasOwnProperty('usage')) {
+                    if(this.config.usage == 'month' || this.config.usage.indexOf('date/month') == 0) {
+                        if(datepickerConfig.hasOwnProperty('dateFormat_month')) {
+                            datepickerConfig.dateFormat = datepickerConfig.dateFormat_month;
+                        }
+                    }
+                }
+                // #memo - in some cases datepicker fails to parse the date with applied format, and falls back to current date as initial value
+                datepickerConfig.defaultDate = date;
+                // convert jquery format to moment format
+                format = datepickerConfig.dateFormat;
+                let formatAdapter : any = {
+                    'dd': 'DD',
+                    'mm': 'MM',
+                    'yy': 'YYYY'
+                };
+                for(const key in formatAdapter) {
+                    let re = new RegExp(key, 'g');
+                    format = format.replace(re, formatAdapter[key]);
+                }
+                value = moment(date).format(format);
+                this.$elem = UIHelper.createInput('date_'+this.id, this.label, value, this.config.description, 'calendar_today', this.readonly);
+                if(this.config.layout == 'list') {
+                    this.$elem.css({"width": "calc(100% - 10px)"});
+                }
+                // setup handler for relaying value update to parent layout
+                this.$elem.find('input')
+                .datepicker(datepickerConfig)
                 .on('change', (event:any) => {
                     // update widget value using jQuery `getDate`
                     let $this = $(event.currentTarget);
-                    let date = $this.datepicker('getDate');
+                    // #memo - in some cases datepicker fails to parse the date with applied format, and falls back to current date
+                    // let date = $this.datepicker('getDate');
+                    let date = moment(<string> $this.val(), format).toDate();
                     // make the date UTC @ 00:00:00
                     let timestamp = date.getTime();
                     let offset_tz = date.getTimezoneOffset()*60*1000;
@@ -56,7 +79,7 @@ export default class WidgetDate extends Widget {
                 break;
             case 'view':
             default:
-                // #todo - adapt and complete based on retrieved locale from equal (@see packages/core/i18n/fr/locale.json)
+                // #todo - adapt and complete based on retrieved locale from equal (@see packages/core/i18n/.../locale.json)
                 if(this.config.hasOwnProperty('usage')) {
                     if(this.config.usage == 'date' || this.config.usage == 'date/medium' || this.config.usage == 'date/plain.medium') {
                         // 06/08/2023
@@ -68,6 +91,9 @@ export default class WidgetDate extends Widget {
                     }
                     else if(this.config.usage == 'date/plain.short.day') {
                         format = 'dd ' + (moment.localeData().longDateFormat('L')).replace(/YYYY/g,'YY');
+                    }
+                    else if(this.config.usage == 'month' || this.config.usage.indexOf('date/month') == 0) {
+                        format = 'MMM YYYY';
                     }
                 }
 
