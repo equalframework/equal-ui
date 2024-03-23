672fe268f2dac9252d42b7affdaae8d88a6d0508
Cédric Françoys <cedricfrancoys@gmail.com>
2024-03-23 11:46:56 +0100

    fixed  actions update upon refresh + syntax

--- src/layouts/LayoutForm.ts
+++ src/layouts/LayoutForm.ts
@@ -70,105 +70,110 @@ export class LayoutForm extends Layout {
                 $group.append($('<div/>').addClass('sb-view-form-group-title').text(group_title));
             }
 
-            let selected_section = 0;
-            if(view_config && view_config.hasOwnProperty('selected_sections') && view_config.selected_sections.hasOwnProperty(i)) {
-                selected_section = view_config.selected_sections[i];
-            }
-
-            let $tabs = UIHelper.createTabBar('sections-'+group_id, '', '').addClass('sb-view-form-sections-tabbar');
-
-            if(group.sections.length > 1 ||  group.sections[0].hasOwnProperty('label')){
-                $group.append($tabs);
-            }
-
-            if(group.hasOwnProperty('visible')) {
-                $group.attr('data-visible', JSON.stringify(group.visible));
-            }
-
-            $.each(group.sections, (j:number, section) => {
-                let section_id = group_id+'-section-'+j;
-
-                let $section = $('<div />').attr('id', section_id).addClass('sb-view-form-section mdc-layout-grid').appendTo($group);
-
-                if(j != selected_section) {
-                    $section.hide();
+            if(group.hasOwnProperty('sections') && group.sections.length > 0) {
+                let selected_section = 0;
+                if(view_config && view_config.hasOwnProperty('selected_sections') && view_config.selected_sections.hasOwnProperty(i)) {
+                    selected_section = view_config.selected_sections[i];
                 }
 
-                if(group.sections.length > 1 || section.hasOwnProperty('label')) {
-                    // try to resolve the section title
-                    let section_title = (section.hasOwnProperty('label'))?section.label:section_id;
-                    if(section.hasOwnProperty('id')) {
-                        section_title = TranslationService.resolve(translation, 'view', [this.view.getId(), 'layout'], section.id, section_title);
-                    }
+                let $tabs = UIHelper.createTabBar('sections-'+group_id, '', '').addClass('sb-view-form-sections-tabbar');
 
-                    let $tab = UIHelper.createTabButton(section_id+'-tab', section_title, (j == selected_section)).addClass('sb-view-form-section-tab')
-                    .on('click', () => {
-                        $group.find('.sb-view-form-section').hide();
-                        $group.find('#'+section_id).show();
-                    });
-
-                    if(section.hasOwnProperty('visible')) {
-                        $tab.attr('data-visible', JSON.stringify(section.visible));
-                    }
-
-                    $tabs.find('.sb-view-form-sections').append($tab);
+                if(group.sections.length > 1 ||  group.sections[0].hasOwnProperty('label')){
+                    $group.append($tabs);
                 }
 
+                if(group.hasOwnProperty('visible')) {
+                    $group.attr('data-visible', JSON.stringify(group.visible));
+                }
 
-                $.each(section.rows, (k:number, row) => {
-                    let $row = $('<div />').addClass('sb-view-form-row mdc-layout-grid__inner').appendTo($section);
-                    $.each(row.columns, (l:number, column) => {
-                        let $column = $('<div />').addClass('mdc-layout-grid__cell').appendTo($row);
+                $.each(group.sections, (j:number, section) => {
+                    let section_id = group_id+'-section-'+j;
 
-                        if(column.hasOwnProperty('width')) {
-                            $column.addClass('mdc-layout-grid__cell--span-' + Math.round((parseInt(column.width, 10) / 100) * 12));
+                    let $section = $('<div />').attr('id', section_id).addClass('sb-view-form-section mdc-layout-grid').appendTo($group);
+
+                    if(j != selected_section) {
+                        $section.hide();
+                    }
+
+                    if(group.sections.length > 1 || section.hasOwnProperty('label')) {
+                        // try to resolve the section title
+                        let section_title = (section.hasOwnProperty('label'))?section.label:section_id;
+                        if(section.hasOwnProperty('id')) {
+                            section_title = TranslationService.resolve(translation, 'view', [this.view.getId(), 'layout'], section.id, section_title);
                         }
 
-                        if(column.hasOwnProperty('align') && column.align == 'right') {
-                            $column.css({'margin-left': 'auto'});
+                        let $tab = UIHelper.createTabButton(section_id+'-tab', section_title, (j == selected_section)).addClass('sb-view-form-section-tab')
+                            .on('click', () => {
+                                $group.find('.sb-view-form-section').hide();
+                                $group.find('#'+section_id).show();
+                            });
+
+                        if(section.hasOwnProperty('visible')) {
+                            $tab.attr('data-visible', JSON.stringify(section.visible));
                         }
 
-                        let $inner_cell = $('<div />').addClass('mdc-layout-grid__cell').appendTo($column);
-                        $column = $('<div />').addClass('mdc-layout-grid__inner').appendTo($inner_cell);
+                        $tabs.find('.sb-view-form-sections').append($tab);
+                    }
 
-                        $.each(column.items, (i, item) => {
-                            if(typeof this.model_widgets[0] == 'undefined') {
-                                this.model_widgets[0] = {};
+
+                    $.each(section.rows, (k:number, row) => {
+                        let $row = $('<div />').addClass('sb-view-form-row mdc-layout-grid__inner').appendTo($section);
+                        $.each(row.columns, (l:number, column) => {
+                            let $column = $('<div />').addClass('mdc-layout-grid__cell').appendTo($row);
+
+                            if(column.hasOwnProperty('width')) {
+                                $column.addClass('mdc-layout-grid__cell--span-' + Math.round((parseInt(column.width, 10) / 100) * 12));
                             }
 
-                            let $cell = $('<div />').addClass('mdc-layout-grid__cell').appendTo($column);
-                            // compute the width (on a 12 columns grid basis), from 1 to 12
-                            let width = (item.hasOwnProperty('width'))?Math.round((parseInt(item.width, 10) / 100) * 12): 12;
-                            $cell.addClass('mdc-layout-grid__cell--span-' + width);
-
-                            if(item.hasOwnProperty('align') && item.align == 'right') {
-                                $cell.css({'margin-left': 'auto'});
+                            if(column.hasOwnProperty('align') && column.align == 'right') {
+                                $column.css({'margin-left': 'auto'});
                             }
 
-                            if(item.hasOwnProperty('type') && item.hasOwnProperty('value')) {
+                            let $inner_cell = $('<div />').addClass('mdc-layout-grid__cell').appendTo($column);
+                            $column = $('<div />').addClass('mdc-layout-grid__inner').appendTo($inner_cell);
 
-                                if(item.type == 'field') {
-                                    let config = WidgetFactory.getWidgetConfig(this.view, item.value, translation, model_fields, view_fields);
-                                    if(config) {
-                                        let widget: Widget = WidgetFactory.getWidget(this, config.type, config.title, '', config);
-                                        widget.setReadonly(config.readonly);
-                                        // store widget in widgets Map, using field name as key
-                                        this.model_widgets[0][item.value] = widget;
-                                        $cell.append(widget.attach());
+                            $.each(column.items, (i, item) => {
+                                if(typeof this.model_widgets[0] == 'undefined') {
+                                    this.model_widgets[0] = {};
+                                }
+
+                                let $cell = $('<div />').addClass('mdc-layout-grid__cell').appendTo($column);
+                                // compute the width (on a 12 columns grid basis), from 1 to 12
+                                let width = (item.hasOwnProperty('width'))?Math.round((parseInt(item.width, 10) / 100) * 12): 12;
+                                $cell.addClass('mdc-layout-grid__cell--span-' + width);
+
+                                if(item.hasOwnProperty('align') && item.align == 'right') {
+                                    $cell.css({'margin-left': 'auto'});
+                                }
+
+                                if(item.hasOwnProperty('type') && item.hasOwnProperty('value')) {
+
+                                    if(item.type == 'field') {
+                                        let config = WidgetFactory.getWidgetConfig(this.view, item.value, translation, model_fields, view_fields);
+                                        if(config) {
+                                            let widget: Widget = WidgetFactory.getWidget(this, config.type, config.title, '', config);
+                                            widget.setReadonly(config.readonly);
+                                            // store widget in widgets Map, using field name as key
+                                            this.model_widgets[0][item.value] = widget;
+                                            $cell.append(widget.attach());
+                                        }
+                                    }
+                                    else if(item.type == 'label') {
+                                        let label_title = TranslationService.resolve(translation, 'view', [this.view.getId(), 'layout'], item.id, item.value);
+                                        let widget:Widget = WidgetFactory.getWidget(this, 'label', '', label_title, {widget_type: 'label', ...item});
+                                        this.model_widgets[0]['__label_'+widget.getId()] = widget;
+                                        $cell.append(widget.render());
                                     }
                                 }
-                                else if(item.type == 'label') {
-                                    let label_title = TranslationService.resolve(translation, 'view', [this.view.getId(), 'layout'], item.id, item.value);
-                                    let widget:Widget = WidgetFactory.getWidget(this, 'label', '', label_title, {widget_type: 'label', ...item});
-                                    this.model_widgets[0]['__label_'+widget.getId()] = widget;
-                                    $cell.append(widget.render());
-                                }
-                            }
+                            });
                         });
                     });
                 });
-            });
-            UIHelper.decorateTabBar($tabs);
+
+                if(group.sections.length > 1) {
+                    UIHelper.decorateTabBar($tabs);
+                }
+            }
         });
 
         this.$layout.append($elem);
@@ -192,7 +197,7 @@ export class LayoutForm extends Layout {
 
             // update actions in view header
             let view_schema = this.view.getViewSchema();
-            let $view_actions = this.view.getContainer().find('.sb-view-header-actions-view');
+            let $view_actions = this.view.getContainer().find('.sb-view-header-actions-view').first();
 
             // show object status, if defined and present
             if(model_fields.hasOwnProperty('status')) {
@@ -248,40 +253,30 @@ export class LayoutForm extends Layout {
                 }
 
                 // retrieve previously created elements, if any
-                let $action_button = $view_actions.find('#'+this.uuid+'_actions-button');
-                let $actions_dropdown = $view_actions.find('#'+this.uuid+'_actions-dropdown');
+                $view_actions.find('#'+this.uuid+'_actions-button').remove();
+                $view_actions.find('#'+this.uuid+'_actions-dropdown').remove();
 
                 // there is a single action: show it as a button
                 if(actions.length == 1) {
                     let action = actions[0];
-                    $actions_dropdown.remove();
-                    if($action_button.length == 0) {
-                        let action_title = TranslationService.resolve(this.view.getTranslation(), 'view', [this.view.getId(), 'actions'], action.id, action.label);
-                        $action_button = UIHelper.createButton(this.uuid+'_actions-button', action_title, 'outlined')
-                        this.decorateActionButton($action_button, action, object);
-                        $view_actions.append($action_button);
-                    }
+                    let action_title = TranslationService.resolve(this.view.getTranslation(), 'view', [this.view.getId(), 'actions'], action.id, action.label);
+                    let $action_button = UIHelper.createButton(this.uuid+'_actions-button', action_title, 'outlined');
+                    $view_actions.append($action_button);
+                    this.decorateActionButton($action_button, action, object);
                 }
                 // there are several actions: display a dropdown
                 else if(actions.length > 1) {
-                    $action_button.remove();
-                    if($actions_dropdown.length == 0) {
-                        $actions_dropdown = UIHelper.createDropDown(this.uuid+'_actions-dropdown', 'Actions', 'text', '', '').addClass('layout-actions');
-                        $view_actions.append($actions_dropdown);
-                    }
-                    let $menu_list = $actions_dropdown.find('.menu-list').empty();
+                    let $actions_dropdown = UIHelper.createDropDown(this.uuid+'_actions-dropdown', 'Actions', 'text', '', '').addClass('layout-actions');
+                    $view_actions.append($actions_dropdown);
+                    let $menu_list = $actions_dropdown.find('.menu-list');
                     // keep track of empty lists
                     for(let action of actions) {
                         let action_title = TranslationService.resolve(this.view.getTranslation(), 'view', [this.view.getId(), 'actions'], action.id, action.label);
-                        let $item = UIHelper.createListItem(this.uuid+'_action-'+action.id.replace('.', '_'), action_title);
-                        this.decorateActionButton($item, action, object);
+                        let item_id = this.uuid+'_action-'+(''+action.id).replace(/\./g,'_');
+                        let $item = UIHelper.createListItem(item_id, action_title);
                         $menu_list.append($item);
+                        this.decorateActionButton($item, action, object);
                     }
-                }
-                else {
-                    // no action available
-                    $action_button.remove();
-                    $actions_dropdown.remove();
                 }
             }
 
