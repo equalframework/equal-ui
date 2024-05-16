903032b903eef47573a0497052d73eca136315ee
Cédric Françoys <cedricfrancoys@gmail.com>
2024-05-16 22:20:09 +0200

    improved number formatting

--- src/EnvService.ts
+++ src/EnvService.ts
@@ -33,7 +33,7 @@ export class _EnvService {
         if(!this.promise) {
             this.promise = new Promise( async (resolve, reject) => {
                 try {
-                    const response:Response = await fetch('/assets/env/config.json');
+                    const response:Response = await fetch('/envinfo');
                     const env = await response.json();
                     this.assignEnv({...this.default, ...env});
                     resolve(this.environment);
@@ -66,32 +66,42 @@ export class _EnvService {
         }
     }
 
-    public formatNumber(value:number, scale:number = 0, thousand_sep:string=',', decimal_sep:string='.') {
-        if(this.environment) {
-            if(this.environment.hasOwnProperty('core.locale.numbers.decimal_precision')) {
+    public formatNumber(value: number, scale: number = -1, thousand_sep: string = '', decimal_sep: string = '') {
+        if(scale == -1) {
+            scale = 0;
+            if(this.environment && this.environment.hasOwnProperty('core.locale.numbers.decimal_precision')) {
                 scale = this.environment['core.locale.numbers.decimal_precision'];
             }
-            if(this.environment.hasOwnProperty('core.locale.numbers.decimal_separator')) {
-                decimal_sep = this.environment['core.locale.numbers.decimal_separator'];
-            }
-            if(this.environment.hasOwnProperty('core.locale.numbers.thousands_separator')) {
+        }
+        if(thousand_sep == '') {
+            thousand_sep = ',';
+            if(this.environment && this.environment.hasOwnProperty('core.locale.numbers.thousands_separator')) {
                 thousand_sep = this.environment['core.locale.numbers.thousands_separator'];
             }
-            let parts:any = value.toFixed(scale).split(".");
-            parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousand_sep);
-            if(scale > 0 && parts.length == 1) {
-                parts[1] = ''.padStart(scale, '0');
-            }
-            return parts.join(decimal_sep);
         }
-        return value.toLocaleString();
+        if(decimal_sep == '') {
+            decimal_sep = '.';
+            if(this.environment && this.environment.hasOwnProperty('core.locale.numbers.decimal_separator')) {
+                decimal_sep = this.environment['core.locale.numbers.decimal_separator'];
+            }
+        }
+        let parts:any = value.toFixed(scale).split(".");
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousand_sep);
+        if(scale > 0 && parts.length == 1) {
+            parts[1] = ''.padStart(scale, '0');
+        }
+        return parts.join(decimal_sep);
     }
 
-    public formatCurrency(value:number, scale:number = 2, thousand_sep:string=',', decimal_sep:string='.') {
+    public formatFinancialNumber(value:number, scale:number = 2, thousand_sep:string=',', decimal_sep:string='.') {
         if(this.environment && this.environment.hasOwnProperty('core.locale.currency.decimal_precision')) {
             scale = this.environment['core.locale.currency.decimal_precision'];
         }
-        let result = this.formatNumber(value, scale, thousand_sep, decimal_sep);
+        return this.formatNumber(value, scale, thousand_sep, decimal_sep);
+    }
+
+    public formatCurrency(value:number, scale:number = 2, thousand_sep:string=',', decimal_sep:string='.') {
+        let result = this.formatFinancialNumber(value, scale, thousand_sep, decimal_sep);
         if(this.environment.hasOwnProperty('core.units.currency')) {
             if(this.environment.hasOwnProperty('core.locale.currency.symbol_position') && this.environment['core.locale.currency.symbol_position'] == 'before') {
                 result = this.environment['core.units.currency'] + ' ' + result;
