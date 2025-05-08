71567588319ba5066ee51f4db04ee48e904ab255
Cédric Françoys <cedricfrancoys@gmail.com>
2025-05-08 08:55:03 +0200

    added other icons (folder, file, document)

--- src/widgets/WidgetString.ts
+++ src/widgets/WidgetString.ts
@@ -94,6 +94,9 @@ export default class WidgetString extends Widget {
                                     error:     {icon: "report", color: "red"},
                                     paid:      {icon: "paid", color: "green"},
                                     due:       {icon: "money_off", color: "red"},
+                                    folder:    {icon: "folder", color: "#F8D775"},
+                                    file:      {icon: "description", color: "grey"},
+                                    document:  {icon: "description", color: "grey"}
                                 };
 
                             this.$elem = $('<div />');
