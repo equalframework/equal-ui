354d265915f5da19f41d560221abd6d4ee193cf7
Cédric Françoys <cedricfrancoys@gmail.com>
2025-05-08 08:59:26 +0200

    added support for storing meta (relayed on onchange)

--- src/widgets/WidgetFile.ts
+++ src/widgets/WidgetFile.ts
@@ -15,7 +15,7 @@ export default class WidgetFile extends Widget {
     }
 
     public render():JQuery {
-        let value:string = (typeof this.value != undefined && this.value != undefined)?this.value:'';
+        let value: string = (typeof this.value != undefined && this.value != undefined) ? this.value : '';
         let $button_open = UIHelper.createButton('link-actions-open-'+this.id, '', 'icon', 'open_in_new');
 
         // open target in new window
@@ -35,35 +35,49 @@ export default class WidgetFile extends Widget {
         switch(this.mode) {
             case 'edit':
                 this.$elem.css({"display": "flex", "align-items": "center"});
-                let $input:JQuery = $('<input type="file" />').hide();
+                let $input: JQuery = $('<input type="file" />').hide();
 
                 let $button = UIHelper.createButton(this.getId()+'_upload-button', TranslationService.instant('SB_ACTIONS_BUTTON_SELECT'), 'raised', '', 'primary').css({"margin-left": "10px"});
 
-                let $text = UIHelper.createInputView('', this.label, '', this.config.description);
+                let $text = UIHelper.createInputView('', this.label, this.meta?.name ?? '', this.config.description);
+
                 $text.on('click', () => $input.trigger('click') );
                 $button.on('click', () => $input.trigger('click') );
 
                 $input.on('change', async (event:any) => {
-                    let val:string = <string>$input.val();
 
-                    this.value = await ( ( blob:any ) => {
-                        let defer = $.Deferred();
-                        var reader = new FileReader();
-                        reader.onload = (e:any) => {
-                            defer.resolve(e.target.result);
-                        };
-                        reader.readAsDataURL(blob);
-                        return defer.promise();
-                    })($input.prop('files')[0]);
+                    let val: string = <string> $input.val();
+
+                    this.value = await ( ( blob: any ) => {
+                            const defer = $.Deferred();
+
+                            this.setMeta({
+                                name: blob.name,
+                                size: blob.size,
+                                type: blob.type,
+                                lastModified: blob.lastModified
+                            });
+
+                            const reader = new FileReader();
+
+                            reader.onload = (e:any) => {
+                                defer.resolve(e.target.result);
+                            };
+
+                            reader.readAsDataURL(blob);
+
+                            return defer.promise();
+                        })
+                        ($input.prop('files')[0]);
 
                     this.$elem.trigger('_updatedWidget', [false]);
 
-                    let filename = <string>val.split('\\').pop();
+                    let filename = <string> val.split('\\').pop();
                     $text.remove();
                     $text = UIHelper.createInputView('', this.label, filename, this.config.description);
                     this.$elem.prepend($text);
-
                 });
+
                 this.$elem.append($text).append($button).append($input);
 
                 break;
