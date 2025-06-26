32db1418a6ed532c93b0994407e465c9898a02c4
Cédric Françoys <cedricfrancoys@gmail.com>
2025-06-26 09:34:12 +0200

    moved support for styling in parent Widget class

--- src/widgets/WidgetLabel.ts
+++ src/widgets/WidgetLabel.ts
@@ -9,64 +9,26 @@ export default class WidgetLabel extends Widget {
         super(layout, label, value, config);
     }
 
-
     public render(): JQuery {
-        let value:any = (typeof this.value != undefined && this.value != undefined)?this.value:'';
+        let value:any = (typeof this.value != undefined && this.value != undefined) ? this.value : '';
         if(typeof value == 'string') {
             value = value.replace(/"/g, "&quot;");
         }
 
         this.$elem = $('<span style="display: inline-block; width: 100%; height: 46px; padding-top: 24px; line-height: 22px;" />');
 
-        let $text = $('<span style="display: inline-block; line-height: 22px;">' + value + '</span>').appendTo(this.$elem);
+        let $text = $('<span style="display: inline-block; line-height: 22px;">' + value + '</span>')
+            // default layout
+            .css({
+                'font-weight': '600',
+                'width': '100%'
+            })
+            .appendTo(this.$elem);
 
-        // default layout
-        $text.css({'font-weight': '600'});
-        $text.css({'width': '100%'});
+        this.applyStyling($text);
 
-
-        if(this.config.hasOwnProperty('text_decoration') && this.config.text_decoration) {
-            if(this.config.text_decoration === 'underline') {
-                $text.css({'border-bottom': 'solid 1px black'});
-            }
-        }
-
-        if(this.config.hasOwnProperty('text_color') && this.config.text_color) {
-            $text.css({'color': this.config.text_color});
-        }
-
-        if(this.config.hasOwnProperty('text_width') && this.config.text_width) {
-            $text.css({
-                    'width': this.normalizeToPx(this.config.text_width),
-                    'min-width': this.normalizeToPx(this.config.text_width)
-                });
-        }
-
-        if(this.config.hasOwnProperty('background_color') && this.config.background_color) {
-            $text.css({'background-color': this.config.background_color});
-        }
-
-        if(this.config.hasOwnProperty('border_radius') && this.config.border_radius) {
-            $text.css({'border-radius': this.normalizeToPx(this.config.text_color)});
-        }
-
-        if(this.config.hasOwnProperty('padding') && this.config.padding) {
-            $text.css({'padding': this.config.padding});
-        }
-
-        if(this.config.hasOwnProperty('margin') && this.config.margin) {
-            $text.css({'margin': this.config.margin});
-        }
-
-        return this.$elem.addClass('sb-widget').addClass('sb-widget-mode-'+this.mode).attr('id', this.getId());
+        return this.$elem.addClass('sb-widget').addClass('sb-widget-mode-' + this.mode).attr('id', this.getId());
     }
 
-    private normalizeToPx(v: any): string {
-        let n = (typeof v === 'number') ? v : parseInt(v as string, 10);
-        if(isNaN(n)) {
-            n = 0;
-        }
-        return `${n}px`;
-    };
 
 }
