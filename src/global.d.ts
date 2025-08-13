26c981eda3f0791ae4439662a0d4c1dc782ea048
Cédric Françoys <cedricfrancoys@gmail.com>
2025-08-13 10:11:07 +0200

    removed quill-better-table (tests)

--- src/global.d.ts
+++ src/global.d.ts
@@ -10,11 +10,3 @@ interface JQuery {
     daterangepicker(options?: any, callback?: Function) : any;
     timepicker(options?: any, callback?: Function) : any;
 }
-
-declare module 'quill-better-table' {
-  import Quill from 'quill';
-
-  export default class QuillBetterTable {
-    constructor(quill: Quill, options: any);
-  }
-}
