ac06850d69d8d2a2dd4ff4586d14136b51b3c853
Lucas <lucas@yesbabylon.com>
2025-08-27 09:33:18 +0200

    fix local for jquery moment conversion when dater ange

--- src/View.ts
+++ src/View.ts
@@ -2417,8 +2417,10 @@ export class View {
                     // daterange selector
                     $select_value = UIHelper.createInput('bulk-assign-select-value_' + this.getUuid(), TranslationService.instant('SB_FILTERS_DIALOG_VALUE'), '');
 
+                    const locale = this.config.locale.slice(0, 2);
+
                     $select_value.find('input').datepicker({
-                        ...jqlocale[this.config.locale],
+                        ...jqlocale[locale],
                         onClose: () => {
                             $select_value.find('input').trigger('focus');
                         }
