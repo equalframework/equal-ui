4d72ac8baccd1278095eb7ca52f7603ca80bf47d
Cédric Françoys <cedricfrancoys@gmail.com>
2026-02-09 10:18:32 +0100

    added adaptIn + adaptOut & support for human notation (e. 10h30)

--- src/widgets/WidgetTime.ts
+++ src/widgets/WidgetTime.ts
@@ -10,17 +10,13 @@ export default class WidgetTime extends Widget {
     }
 
     public change(value: any) {
-        this.$elem.find('input').val(value).trigger('change');
+        this.$elem.find('input').val(this.adaptIn(value)).trigger('change');
+        return this;
     }
 
-    public render():JQuery {
-        // #todo - adaptIN - times should always be UTC - we should apply time offset here
-        let value:any = this.value ?? '';
-        // if we received a date, convert it to a string
-        if(Object.prototype.toString.call(value) === '[object Date]') {
-            // #todo - adaptIN - we should rather use `toUTCString()`
-            value = value.toTimeString().substring(0, 5);
-        }
+    public render(): JQuery {
+        // adaptIN - times are always UTC - (browser time offset is applied)
+        let value: any = this.adaptIn(this.value ?? '');
 
         switch(this.mode) {
             case 'edit':
@@ -31,12 +27,11 @@ export default class WidgetTime extends Widget {
 
                 // #memo - not dealing with keydown is preferred to avoid confusion about special keys role
                 // #memo - we use 'change' event to cover float and integers changes with up and down buttons (same timeout)
-                this.$elem.find('input').on('change', (event:any) => {
-                    let $this = $(event.currentTarget);
-
-                    // #todo - adapt-OUT - times should always be UTC - we should apply time offset here
-                    if(this.value != $this.val()) {
-                        this.value = $this.val();
+                this.$elem.find('input').on('change', (event: any) => {
+                    // adapt-OUT - times are always be UTC
+                    const newValue = this.adaptOut();
+                    if(this.value !== newValue) {
+                        this.value = newValue;
                         this.$elem.trigger('_updatedWidget', [false]);
                     }
                 });
@@ -44,7 +39,6 @@ export default class WidgetTime extends Widget {
                 break;
             case 'view':
             default:
-                value = this.toString();
                 if(this.config.layout == 'list') {
                     this.$elem = $('<div />').html(value);
                     // time fields are aligned right
@@ -74,6 +68,78 @@ export default class WidgetTime extends Widget {
             .attr('data-type', this.config.type)
             .attr('data-field', this.config.field)
             .attr('data-usage', this.config.usage || '');
+    }
+
+    /**
+     * Adapt UTC time (HH:mm[:ss]) to local time (HH:mm)
+     */
+    private adaptIn(value: any): string {
+        if (!value) {
+            return '';
+        }
+
+        // normalize human time formats like "10h30", "10 h", "10 h 30"
+        value = value
+            .trim()
+            .toLowerCase()
+            .replace(/\s+/g, '')
+            .replace(/^(\d{1,2})h(\d{0,2})$/, (match: string, h: string, m: string) => {
+                    const hh = h.padStart(2, '0');
+                    const mm = m.padStart(2, '0');
+                    return `${hh}:${mm}`;
+                }
+            );
+
+        // Date object assumed UTC
+        if (Object.prototype.toString.call(value) === '[object Date]') {
+            const h = value.getUTCHours().toString().padStart(2, '0');
+            const m = value.getUTCMinutes().toString().padStart(2, '0');
+            return `${h}:${m}`;
+        }
+
+        if (typeof value !== 'string') {
+            return '';
+        }
+
+        const parts = value.split(':').map(Number);
+        const h = parts[0] ?? 0;
+        const m = parts[1] ?? 0;
+        const s = parts[2] ?? 0;
+
+        const now = new Date();
+        const utcDate = new Date(Date.UTC(
+            now.getUTCFullYear(),
+            now.getUTCMonth(),
+            now.getUTCDate(),
+            h, m, s
+        ));
+
+        const lh = utcDate.getHours().toString().padStart(2, '0');
+        const lm = utcDate.getMinutes().toString().padStart(2, '0');
+
+        return `${lh}:${lm}`;
+    }
+
+    /**
+     * Adapt local time (HH:mm) to UTC time (HH:mm:ss)
+     */
+    private adaptOut(): string {
+        const input = this.$elem?.find('input').val();
+        if (!input) return '';
+
+        const parts = String(input).split(':').map(Number);
+        const h = parts[0] ?? 0;
+        const m = parts[1] ?? 0;
+        const s = parts[2] ?? 0;
+
+        const localDate = new Date();
+        localDate.setHours(h, m, s, 0);
+
+        const uh = localDate.getUTCHours().toString().padStart(2, '0');
+        const um = localDate.getUTCMinutes().toString().padStart(2, '0');
+        const us = localDate.getUTCSeconds().toString().padStart(2, '0');
+
+        return `${uh}:${um}:${us}`;
     }
 
 }
