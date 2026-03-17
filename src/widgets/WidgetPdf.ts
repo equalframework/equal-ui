a7b65c5f15e2f6d991ced9ea8a08ad4716308856
Cédric Françoys <cedricfrancoys@gmail.com>
2026-03-17 11:45:25 +0100

    improved ppositionning of PDF controls

--- src/widgets/WidgetPdf.ts
+++ src/widgets/WidgetPdf.ts
@@ -23,12 +23,12 @@ export default class WidgetPdf extends Widget {
                 }
                 else {
                     this.$elem = $('<div class="sb-ui-pdfjs" />');
-                    this.$elem.append( $('<iframe id=""width="100%" height="100%" allowfullscreen style="border: none;" />').attr('src', '/pdfjs/web/viewer.html?file=' + value) );
+                    this.$elem.append( $('<iframe id="' + 'widget-pdf_iframe_' + this.getId() + '" width="100%" height="100%" allowfullscreen style="border: none;" />').attr('src', '/pdfjs/web/viewer.html?file=' + value) );
 
                     if(this.config.hasOwnProperty('height') && this.config.height > 0) {
                         this.$elem.css({height: this.config.height + 'px'});
                     }
-                    this.addFullscreenButton(this.$elem);
+                    this.initializePdfControls(this.$elem);
                 }
                 break;
         }
@@ -43,18 +43,11 @@ export default class WidgetPdf extends Widget {
     }
 
 
-    private addFullscreenButton($elem: JQuery) {
+    private initializePdfControls($elem: JQuery) {
         let $button = $('<div><i class="fa fa-expand"></i></div>').addClass('pdfjs-custom-fullscreen');
 
         // #memo - we assume there is max 1 doc per page
         const $iframe = $elem.find('iframe').first();
-
-        function placeFullscreenButton() {
-            let rect = $iframe[0].getBoundingClientRect();
-            let scroll_top = Math.floor($(window).scrollTop() || 0);
-            $button.css({top: scroll_top + rect.top + 1 + 'px', left: (rect.left + rect.width - 30 - 10) + 'px'});
-            requestAnimationFrame(placeFullscreenButton);
-        }
 
         // setup button onclick listener
         if(!$iframe.length) {
@@ -74,10 +67,9 @@ export default class WidgetPdf extends Widget {
                     $iframe.contents().find("#toolbarViewerMiddle").css("display", "none");
                 }
 
-                $('.pdfjs-custom-fullscreen').remove();
+                $elem.find('.pdfjs-custom-fullscreen').remove();
 
-                $button.prependTo('body');
-                placeFullscreenButton();
+                $iframe.before($button);
 
                 $button.on('click', function (e) {
                     const state = $button.attr('data-state');
@@ -85,17 +77,15 @@ export default class WidgetPdf extends Widget {
                     console.log('button state', state);
                     if(!state || state == 'collapsed') {
                         $button.attr('data-state', 'expanded');
-                        $('body').css({'overflow': 'hidden'});
-                        $iframe.addClass('pdfjs-custom-iframe-fullscreen');
-                        $button.find('i').addClass('fa-compress');
-                        $button.find('i').removeClass('fa-expand');
+                        $('body').toggleClass('no-scroll');
+                        $elem.addClass('fullscreen');
+                        $button.find('i').toggleClass('fa-expand fa-compress');
                     }
                     else {
                         $button.attr('data-state', 'collapsed');
-                        $('body').css({'overflow': 'auto'});
-                        $iframe.removeClass('pdfjs-custom-iframe-fullscreen');
-                        $button.find('i').addClass('fa-expand');
-                        $button.find('i').removeClass('fa-compress');
+                        $('body').toggleClass('no-scroll');
+                        $elem.removeClass('fullscreen');
+                        $button.find('i').toggleClass('fa-expand fa-compress');
                     }
                     $button.show();
                 });
