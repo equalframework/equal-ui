c4596c6ea99f20150d5acae17fc2a30fcb2e54da
Cédric Françoys <cedricfrancoys@gmail.com>
2026-08-24 15:39:30 +0200

    removed usage of user.language as locale

--- src/EventsListener.ts
+++ src/EventsListener.ts
@@ -333,10 +333,10 @@ class EventsListener {
             this.user = await ApiService.getUser();
 
             if(this.user.hasOwnProperty('language')) {
-                // #memo - user.language is a locale
-                EnvService.setEnv('locale', this.user.language);
-                // #todo @to_confirm
                 EnvService.setEnv('lang', this.user.language.slice(0, 2));
+                if(this.user.hasOwnProperty('locale')) {
+                    EnvService.setEnv('locale', this.user.locale);
+                }
                 TranslationService.init();
             }
 
